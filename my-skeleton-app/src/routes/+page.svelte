55efe5a0ea13f0c67ae08d122559183f4262d5a2
Morgan <121578632+morganharris39@users.noreply.github.com>
2024-10-02 17:50:18 -0600

    uploaded svelte component 2

---
+++ my-skeleton-app/src/routes/+page.svelte
@@ -0,0 +1,7 @@
+<script>
+	import MyInput from './input.svelte'
+</script>
+
+<MyInput style="default" />
+<MyInput style="invalid" />
+<MyInput style="disabled" />
---
+++ my-skeleton-app/src/routes/input.svelte
@@ -0,0 +1,30 @@
+<script>
+	import '../app.postcss';
+	export let style = "default"
+	export let placeholder = "Enter email"
+</script>
+
+{#if style=="default"}
+	<label class="block">
+		<span class="ml-5 mt-5 block text-md font-large text-slate-900">Email</span>
+		<input type=text value={placeholder} class="ml-5 mb-5 rounded-md border-1 border-gray-300 
+		focus:outline-none focus:border-indigo-700 focus:ring-1 focus:ring-indigo-700">
+	</label>
+{/if}
+
+{#if style=="invalid"}
+	<label class="block">
+		<span class="ml-5 block text-md font-large text-slate-900">Email</span>
+		<input type=text value={placeholder} class="ml-5 mb-5 rounded-md border-1 border-red-500 text-red-500
+		focus:outline-none focus:border-red-500 focus:ring-1 focus:ring-red-500">
+	</label>
+{/if}
+
+{#if style=="disabled"}
+	<label class="block">
+		<span class="ml-5 block text-md font-large text-slate-900">Email</span>
+		<input disabled type=text value={placeholder} class="ml-5 mb-5 rounded-md border-1 border-none bg-gray-200 text-gray-500">
+	</label>
+{/if}
+
+<slot />
